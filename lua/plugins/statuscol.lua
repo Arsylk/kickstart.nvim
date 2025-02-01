eba2e5c4aa5a00b9c0c8dfbb397f6a8e32ed152f
Iwaniuk Krzysztof <iwaniuk.krzysztof@outlook.com>
2025-02-01 18:25:04 +0100

    up

--- lua/plugins/statuscol.lua
+++ lua/plugins/statuscol.lua
@@ -1,18 +1,18 @@
 return {
   {
     'luukvbaal/statuscol.nvim',
-    config = function()
+    opts = function()
       local builtin = require 'statuscol.builtin'
       return {
         relculright = true,
         ft_ignore = { 'neo-tree', 'neo-tree-popup', 'alpha', 'lazy', 'mason', 'dashboard' },
         segments = {
-          { text = { builtin.lnumfunc }, click = 'v:lua.ScLa' },
+          --   { text = { builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
           {
             sign = { name = { 'Diagnostic*' }, text = { '.*' }, maxwidth = 1, colwidth = 1, auto = true },
             click = 'v:lua.ScSa',
           },
-          { text = { builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
+          { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
         },
       }
     end,
