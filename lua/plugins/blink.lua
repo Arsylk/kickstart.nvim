75b73bf68eaf147a5f48337a495d2f1c99b6c5d1
Arsylk <iwaniuk.krzysztof@outlook.com>
2025-04-03 09:32:04 +0200

    hi

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -43,7 +43,7 @@ local kinds = {
 return {
   {
     'saghen/blink.cmp',
-    dependencies = { 'rafamadriz/friendly-snippets', 'xzbdmw/colorful-menu.nvim' },
+    dependencies = { 'rafamadriz/friendly-snippets', 'xzbdmw/colorful-menu.nvim', 'L3MON4D3/LuaSnip' },
     version = '*',
 
     ---@module 'blink.cmp'
@@ -166,6 +166,22 @@ return {
           },
         },
         sources = { 'cmdline' },
+      },
+
+      snippets = {
+        preset = 'luasnip',
+        expand = function(snippet)
+          require('luasnip').lsp_expand(snippet)
+        end,
+        active = function(filter)
+          if filter and filter.direction then
+            return require('luasnip').jumpable(filter.direction)
+          end
+          return require('luasnip').in_snippet()
+        end,
+        jump = function(direction)
+          require('luasnip').jump(direction)
+        end,
       },
     },
 
